45f24ef235d388755367a66262b30661e8b1dd86
Nghi <phamtonghicoder96@gmail.com>
2021-04-28 17:22:09 +0700

    build (_header) build header section continue , add mixins (line, border radius), container, add search icon
    build header section continue with reponsive
    add line, border radius in mixins
    change color name in _variables
    add style for a, ul, p, ol in _common
    add container in _main
    add search icon
    
    Close

---
+++ assets/js/main.js
@@ -0,0 +1,84 @@
+/**
+ * Show inner__collapse when click Hamburger button
+ */
+function hamburgerFunction() {
+    var x = document.getElementById("my__navbar");
+    if (x.className === "inner__collapse") {
+        x.className += " show";
+    }
+    else {
+        x.className = "inner__collapse";
+    }
+    $("a[href^='#']").on('click', function (e) {
+        e.preventDefault();
+        $('#my__navbar').removeClass("show");
+    });
+}
+
+$(function() {
+
+    var navHeight = $(".navbar").height();
+
+    // Event when click search id
+    $(".inner__search").on('click', function(event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Show input text
+            $("#search").addClass('active');
+        }
+        //End if
+    });
+
+  /*  // Event when click anywhere without search id
+    // Event when click search id
+    $(document).on('click', function(event) {
+        console.log('HAHAHA');
+        if (!$(event.target).closest(".inner__search")) {
+            console.log('hash');
+
+            // Hide input text.
+            $("#search").removeClass('active');
+        }
+    });*/
+
+    /**
+     * Add smooth scrolling to all links
+     */
+    $(".nav__link").on('click', function(event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+
+            var refElement = $($(this).attr("href"));
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            $("a").each(function () {
+                $(this).removeClass('active');
+            });
+
+            $(this).addClass('active');
+
+
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (500) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top - navHeight
+            }, 500, 'swing', function () {
+                $(document).on("scroll", onScroll);
+            });
+        }
+        //End if
+    });
+});
